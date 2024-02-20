a038d204e37f145b98425c58217c6eb38d39470f
CorentinD <116890381+Cocochdou@users.noreply.github.com>
2024-02-20 11:51:27 +0100

    Update solve_mastermind_BAYLE_DOUCHE.py
    AHAH

--- solve_mastermind_BAYLE_DOUCHE.py
+++ solve_mastermind_BAYLE_DOUCHE.py
@@ -7,7 +7,11 @@ Created on Mon Feb 21 11:24:15 2022
 Template for exercise 1
 (genetic algorithm module specification)
 """
-import mastermind as mm
+
+
+import mastermind as mm ##C: Importing the mastermind module##
+MATCH = mm.MastermindMatch(secret_size=4) ##C: Selecting the mastermind match to find##
+
 
 class Individual:
     """Represents an Individual for a genetic algorithm"""
@@ -44,9 +48,13 @@ class GASolver:
         self._population = []
 
     def reset_population(self, pop_size=50):
-        """ Initialize the population with pop_size random Individuals """
-        pass  # REPLACE WITH YOUR CODE
-
+        ##C: Adding the population creation##
+        for i in range(pop_size): ##C: Creating pop_size different individuals##
+            chromosome = MATCH.generate_random_guess()
+            fitness = MATCH.rate_guess(chromosome)
+            new_individual = Individual(chromosome, fitness) ##C: Combining fitness + chromosome in indiv##
+            self._population.append(new_individual) ##C: Adding them to the population##
+        pass  
     def evolve_for_one_generation(self):
         """ Apply the process for one generation : 
             -	Sort the population (Descending order)
@@ -57,6 +65,31 @@ class GASolver:
                 mutation_rate i.e., mutate it if a random value is below   
                 mutation_rate
         """
+        
+        ##C: Sort the population##
+        min = self._population[0].fitness
+        for i in range(len(self._population)):
+            for j in range(len(self._population)-i-1):
+                if   self._population[j].__lt__(self._population[j+1]): ##C: Checking if j individual fits less than j+1 one##
+                    self._population[j], self._population[j+1] = self._population[j+1], self._population ##C: Then swap them if its the case##
+        
+        ##C:Selection##
+        popped_out = int(self._selection_rate*len(self._population)) ##C: The number of individual to pop##
+        for i in range(popped_out):
+            self._population.pop() ##Supressing the last element of the array popped_out times ##
+        ##Reproduction: we need to create popped_out new elements that are merge of fittest individuals in the population,
+        ## Our first idea is to create an Alpha Male, the fittest individuals will inseminate a certain portion of individual, giving it first half to the 2nd best individuals, then the third etc...##
+        new_born_counter = 0
+        while new_born_counter != popped_out:
+            for i in (len(self._population)):
+                for j in (len(self._population)-i-1):
+                    if i != j:
+                        cross_chromosome = [self._population[i].chromosome[0],self._population[i].chromosome[1],self._population[j].chromosome[2],self._population[j].chromosome[3]]
+
+
+
+                
+
         pass  # REPLACE WITH YOUR CODE
 
     def show_generation_summary(self):
